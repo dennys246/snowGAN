d49457535e79fe0615818302e6c98a7003e93de7
dennys246 <dennys@rmdig.ai>
2025-08-05 18:37:38 -0600

    Fixing syntax issues, adding in a few new configuration variables for more comprehensive saving and resupmtion of a previous training

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ config_template = {
             "validation_pool": None,
             "test_pool": None,
             "model_history": None,
+            "synthetics": 10,
             "epochs": 10,
             "current_epoch": 0,
             "batch_size": 8,
@@ -24,11 +25,13 @@ config_template = {
             "filter_counts": [64, 128, 256, 512, 1024],
             "kernel_size": [5, 5],
             "kernel_stride": (2, 2),
-            "final_activation": "tahn",
+            "final_activation": "tanh",
             "zero_padding": None,
             "padding": "same",
             "optimizer": "adam",
             "loss": None,
+            "train_ind": 0,
+            "trained_data": [],
             "rebuild": False
 }
 
@@ -45,7 +48,7 @@ class build:
 
         self.configure(**config_json) # Build configuration
 
-        atexit(self.save_config)
+        atexit.register(self.save_config)
 
         
     def __repr__(self):
@@ -69,7 +72,7 @@ class build:
             config_json = config_template
         return config_json
 
-    def configure(self, save_dir, dataset, architecture, resolution, images, trained_pool, validation_pool, test_pool, model_history, epochs, current_epoch, batch_size, training_steps, learning_rate, beta_1, beta_2, alpha, latent_dim, convolution_depth, filter_counts, kernel_size, kernel_stride, final_activation, zero_padding, padding, optimizer, loss, rebuild):
+    def configure(self, save_dir, dataset, architecture, resolution, images, trained_pool, validation_pool, test_pool, model_history, synthetics, epochs, current_epoch, batch_size, training_steps, learning_rate, beta_1, beta_2, alpha, latent_dim, convolution_depth, filter_counts, kernel_size, kernel_stride, final_activation, zero_padding, padding, optimizer, loss, train_ind, trained_data, rebuild):
 		#-------------------------------- Model Set-Up -------------------------------#
         self.save_dir = save_dir
         self.dataset = dataset
@@ -80,15 +83,16 @@ class build:
         self.validation_pool = validation_pool
         self.test_pool = test_pool
         self.model_history = model_history
+        self.synthetics = synthetics
         self.epochs = epochs
         self.current_epoch = current_epoch
         self.batch_size = batch_size
-        self.training_steps = training_steps,
+        self.training_steps = training_steps
         self.learning_rate = learning_rate
         self.beta_1 = beta_1
         self.beta_2 = beta_2
-        self.alpha = alpha,	
-        self.latent_dim = latent_dim,
+        self.alpha = alpha
+        self.latent_dim = latent_dim
         self.convolution_depth = convolution_depth
         self.filter_counts = filter_counts
         self.kernel_size = kernel_size
@@ -98,6 +102,8 @@ class build:
         self.padding = padding
         self.optimizer = optimizer
         self.loss = loss
+        self.train_ind = train_ind
+        self.trained_data = trained_data
         self.rebuild = rebuild
 
     def dump(self):
@@ -111,6 +117,7 @@ class build:
             "validation_pool": self.validation_pool,
             "test_pool": self.test_pool,
             "model_history": self.model_history,
+            "synthetics": self.synthetics,
             "epochs": self.epochs,
             "current_epoch": self.current_epoch,
             "batch_size": self.batch_size,
@@ -129,6 +136,8 @@ class build:
             "padding": self.padding,
             "optimizer": self.optimizer,
             "loss": self.loss,
+            "train_ind": self.train_ind,
+            "trained_data": self.trained_data,
             "rebuild": self.rebuild
         }
         return config
